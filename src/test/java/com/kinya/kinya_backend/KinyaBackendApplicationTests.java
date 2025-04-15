98f9e64e796937126cf83157c728036f00e5b9cf
yll0rd <leoyoumbi83@gmail.com>
2025-04-15 09:21:41 +0200

    initial setup

---
+++ src/test/java/com/kinya/kinya_backend/KinyaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kinya.kinya_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinyaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
