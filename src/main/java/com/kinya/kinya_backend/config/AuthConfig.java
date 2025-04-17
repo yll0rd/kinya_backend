236ac4b3247a4bf0eafcad70740dc48b4170a618
yll0rd <leoyoumbi83@gmail.com>
2025-04-17 15:20:48 +0200

    feat: enable CORS configuration for frontend integration
    Added WebConfig to configure CORS and allow requests from the frontend. Updated AuthConfig to include default CORS handling. Introduced a new property in application.properties to define the frontend URL.
    
    Signed-off-by: yll0rd <leoyoumbi83@gmail.com>

--- src/main/java/com/kinya/kinya_backend/config/AuthConfig.java
+++ src/main/java/com/kinya/kinya_backend/config/AuthConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -28,6 +29,7 @@ public class AuthConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/api/v1/auth/*").permitAll()
---
+++ src/main/java/com/kinya/kinya_backend/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.kinya.kinya_backend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Value("${frontend.url}")
+    private String CORS_ORIGIN;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                System.out.println("Cors configured");
+                registry.addMapping("/**")
+                        .allowedOrigins(CORS_ORIGIN) // Angular server
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
